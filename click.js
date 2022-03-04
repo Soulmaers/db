d282894622d82540020ade487ea7ff152d2008b5
Soulmaers <certiss@ya.ru>
2022-03-04 08:28:51 +0300

    радиокнопки локалстор?

--- click.js
+++ click.js
@@ -39,7 +39,7 @@ link_clear.onclick = function () {
     localStorage.setItem("count_clear", JSON.stringify(count_clear));
 
 };*/
-
+// кнопки замер и продажи+радио кнопки, сохранение в локалстор и удаление
 let order = (n) => localStorage.setItem('c', view_order.textContent = n || 0)
 order(localStorage.getItem('c'));
 link_order.onclick = e => order(+view_order.textContent + 1)
@@ -53,8 +53,50 @@ sale(localStorage.getItem('j'));
 link_sale.onclick = function (i) {
     sale(+view_sale.textContent + 1)
 }
+
+
+
+
 link_clear.onclick = function (i) {
     sale(0);
-    order(0)
+    order(0);
+    fullM(0);
+    fullS(0);
+    aboxsbo(0);
+    biodekasbo(0)
 };
 
+//радион-кнопки, логика выбора
+let count1=count2=count3=count4=0;
+        
+        let radio = document.getElementsByName("dzen");
+        btn.onclick = function displayRadioValue() {
+
+            for (i = 0; i < radio.length; i++) {
+                if (radio[i].checked && radio[i]===full)
+                count1++;
+                    full_view_montaj.textContent = count1;
+                if (radio[i].checked && radio[i]===station)
+                count2++;
+                    station_view_montaj.textContent = count2;
+                if (radio[i].checked && radio[i]===abox)
+                count3++;
+                aboxView.textContent = count3;
+                if (radio[i].checked && radio[i]===biodeka)
+                count4++;
+                biodekaView.textContent = count4;   
+            }
+        };
+
+        let fullM = (n) => localStorage.setItem('a', full_view_montaj.textContent = n || 0)
+        fullM(localStorage.getItem('a'));
+        btn.onclick = e => fullM(+full_view_montaj.textContent + 1);
+        let fullS = (n) => localStorage.setItem('b', station_view_montaj.textContent = n || 0)
+        fullS(localStorage.getItem('b'));
+        btn.onclick = e => fullS(+station_view_montaj.textContent + 1);
+        let aboxsbo = (n) => localStorage.setItem('d', aboxView.textContent = n || 0)
+        aboxsbo(localStorage.getItem('d'));
+        btn.onclick = e => aboxsbo(+aboxView.textContent + 1);
+        let biodekasbo = (n) => localStorage.setItem('y', biodekaView.textContent = n || 0)
+        biodekasbo(localStorage.getItem('y'));
+        btn.onclick = e => biodekasbo(+biodekaView.textContent + 1);
