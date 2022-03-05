bd83f249798e4d925fb69cae41853675d014da5e
Soulmaers <certiss@ya.ru>
2022-03-05 15:01:06 +0300

    Кнопки и радиокнопки готовы

--- click.js
+++ click.js
@@ -39,22 +39,19 @@ link_clear.onclick = function () {
     localStorage.setItem("count_clear", JSON.stringify(count_clear));
 
 };*/
+
+
 // кнопки замер и продажи+радио кнопки, сохранение в локалстор и удаление
 let order = (n) => localStorage.setItem('c', view_order.textContent = n || 0)
 order(localStorage.getItem('c'));
 link_order.onclick = e => order(+view_order.textContent + 1)
-
-
 let sale = function (n) {
     localStorage.setItem('j', view_sale.textContent = n || 0)
 };
 sale(localStorage.getItem('j'));
-
 link_sale.onclick = function (i) {
     sale(+view_sale.textContent + 1)
 }
-
-
 
 
 link_clear.onclick = function (i) {
@@ -63,40 +60,101 @@ link_clear.onclick = function (i) {
     fullM(0);
     fullS(0);
     aboxsbo(0);
-    biodekasbo(0)
+    biodekasbo(0);
+    optimasbo(0);
+    evrolossbo(0);
+    astrasbo(0);
+    topassbo(0);
+    tversbo(0);
+    genezissbo(0);
+    evrobionsbo(0);
+    othersbo(0);
+    addSalessbo(0);
+    spbRegion(0);
+    mskRegion(0);
 };
 
+let fullM = (n) => localStorage.setItem('a', full_view_montaj.textContent = n || 0)
+fullM(localStorage.getItem('a'));
+let mtView = (e) => fullM(+full_view_montaj.textContent + 1);
+let fullS = (n) => localStorage.setItem('b', station_view_montaj.textContent = n || 0)
+fullS(localStorage.getItem('b'));
+let stView = (e) => fullS(+station_view_montaj.textContent + 1);
+let aboxsbo = (n) => localStorage.setItem('d', aboxView.textContent = n || 0)
+aboxsbo(localStorage.getItem('d'));
+let arboxView = e => aboxsbo(+aboxView.textContent + 1);
+let biodekasbo = (n) => localStorage.setItem('y', biodekaView.textContent = n || 0)
+biodekasbo(localStorage.getItem('y'));
+let bdView = e => biodekasbo(+biodekaView.textContent + 1);
+let optimasbo = (n) => localStorage.setItem('r', optimaView.textContent = n || 0)
+optimasbo(localStorage.getItem('r'));
+let oView = e => optimasbo(+optimaView.textContent + 1);
+let evrolossbo = (n) => localStorage.setItem('w', evrolosView.textContent = n || 0)
+evrolossbo(localStorage.getItem('w'));
+let evView = e => evrolossbo(+evrolosView.textContent + 1);
+let astrasbo = (n) => localStorage.setItem('m', astraView.textContent = n || 0)
+astrasbo(localStorage.getItem('m'));
+let asView = e => astrasbo(+astraView.textContent + 1);
+let topassbo = (n) => localStorage.setItem('l', topasView.textContent = n || 0)
+topassbo(localStorage.getItem('l'));
+let tView = e => topassbo(+topasView.textContent + 1);
+let tversbo = (n) => localStorage.setItem('u', tverView.textContent = n || 0)
+tversbo(localStorage.getItem('u'));
+let tvView = e => tversbo(+tverView.textContent + 1);
+let genezissbo = (n) => localStorage.setItem('p', genezisView.textContent = n || 0)
+genezissbo(localStorage.getItem('p'));
+let genView = e => genezissbo(+genezisView.textContent + 1);
+let evrobionsbo = (n) => localStorage.setItem('z', evrobionView.textContent = n || 0)
+evrobionsbo(localStorage.getItem('z'));
+let bionView = e => evrobionsbo(+evrobionView.textContent + 1);
+let othersbo = (n) => localStorage.setItem('f', otherView.textContent = n || 0)
+othersbo(localStorage.getItem('f'));
+let othView = e => othersbo(+otherView.textContent + 1);
+let addSalessbo = (n) => localStorage.setItem('g', addSalesView.textContent = n || 0)
+addSalessbo(localStorage.getItem('g'));
+let addSView = e => addSalessbo(+addSalesView.textContent + 1);
+let spbRegion = (n) => localStorage.setItem('spb', spbView.textContent = n || 0)
+spbRegion(localStorage.getItem('spb'));
+let spbV = e => spbRegion(+spbView.textContent + 1);
+let mskRegion = (n) => localStorage.setItem('msk', mskView.textContent = n || 0)
+mskRegion(localStorage.getItem('msk'));
+let mskV = e => mskRegion(+mskView.textContent + 1);
+
 //радион-кнопки, логика выбора
-let count1=count2=count3=count4=0;
-        
-        let radio = document.getElementsByName("dzen");
-        btn.onclick = function displayRadioValue() {
+let radio = document.getElementsByName("dzen");
+btn.onclick = function displayRadioValue() {
+    for (i = 0; i < radio.length; i++) {
+        if (radio[i].checked && radio[i] === full)
+            mtView();
+        if (radio[i].checked && radio[i] === station)
+            stView();
+        if (radio[i].checked && radio[i] === abox)
+            arboxView();
+        if (radio[i].checked && radio[i] === biodeka)
+            bdView();
+        if (radio[i].checked && radio[i] === optima)
+            oView();
+        if (radio[i].checked && radio[i] === evrolos)
+            evView();
+        if (radio[i].checked && radio[i] === astra)
+            asView();
+        if (radio[i].checked && radio[i] === topas)
+            tView();
+        if (radio[i].checked && radio[i] === tver)
+            tvView();
+        if (radio[i].checked && radio[i] === genezis)
+            genView();
+        if (radio[i].checked && radio[i] === evrobion)
+            bionView();
+        if (radio[i].checked && radio[i] === other)
+            othView();
+        if (radio[i].checked && radio[i] === addSales)
+            addSView();
+        if (radio[i].checked && radio[i] === regionSPB)
+            spbV();
+        if (radio[i].checked && radio[i] === regionMSK)
+            mskV();
 
-            for (i = 0; i < radio.length; i++) {
-                if (radio[i].checked && radio[i]===full)
-                count1++;
-                    full_view_montaj.textContent = count1;
-                if (radio[i].checked && radio[i]===station)
-                count2++;
-                    station_view_montaj.textContent = count2;
-                if (radio[i].checked && radio[i]===abox)
-                count3++;
-                aboxView.textContent = count3;
-                if (radio[i].checked && radio[i]===biodeka)
-                count4++;
-                biodekaView.textContent = count4;   
-            }
-        };
+    }
+};
 
-        let fullM = (n) => localStorage.setItem('a', full_view_montaj.textContent = n || 0)
-        fullM(localStorage.getItem('a'));
-        btn.onclick = e => fullM(+full_view_montaj.textContent + 1);
-        let fullS = (n) => localStorage.setItem('b', station_view_montaj.textContent = n || 0)
-        fullS(localStorage.getItem('b'));
-        btn.onclick = e => fullS(+station_view_montaj.textContent + 1);
-        let aboxsbo = (n) => localStorage.setItem('d', aboxView.textContent = n || 0)
-        aboxsbo(localStorage.getItem('d'));
-        btn.onclick = e => aboxsbo(+aboxView.textContent + 1);
-        let biodekasbo = (n) => localStorage.setItem('y', biodekaView.textContent = n || 0)
-        biodekasbo(localStorage.getItem('y'));
-        btn.onclick = e => biodekasbo(+biodekaView.textContent + 1);
