63cd0a8c9646b4493dc24de2fbefc25f0294d8a3
Soulmaers <certiss@ya.ru>
2022-04-29 15:52:55 +0300

    поле задач

--- click.js
+++ click.js
@@ -14,7 +14,7 @@ link_order.onclick = e => order(+view_order.textContent + 1)
 
 let sale = function (n) {
     localStorage.setItem('j', view_sale.textContent = n || 0)
-    if (n >= 6) {
+    if (n >= 8) {
         document.getElementById("view_sale").style.backgroundColor = '#99C262';
     } else {
         document.getElementById("view_sale").style.backgroundColor = '#f2a5a5';
@@ -202,3 +202,59 @@ btn.onclick = function displayRadioValue() {
     }
 };
 
+
+//поле задач
+
+const inputTask = document.querySelector('#task__input');
+const inputButton = document.querySelector('#tasks__add');
+const taskList = document.querySelector('#tasks__list');
+
+function saveData() {
+    localStorage.setItem('data', taskList.innerHTML)
+}
+
+function addHTML() {
+    if (inputTask.value.trim() != '') {
+        let task = inputTask.value;
+        taskList.innerHTML += `<div class="task">
+                                <div class="task__title">
+                                    ${task}
+                                </div>
+                                <a href="#" class="task__remove">&times;</a>
+                            </div>`
+        inputTask.value = '';
+        saveData();
+    }
+}
+
+function addTask(e) {
+    e.preventDefault();
+    if (e.keyCode === 13) {
+        addHTML();
+        removeTask();
+        saveData();
+    }
+}
+
+function removeTask() {
+    taskRemove = [...taskList.querySelectorAll('.task__remove')]
+    for (let task of taskRemove) {
+        task.addEventListener('click', (e) => {
+            e.preventDefault();
+            e.target.closest('.task').remove();
+            saveData();
+        })
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('data') !== null) {
+        taskList.innerHTML += localStorage.getItem('data');
+        removeTask();
+    }
+})
+inputTask.addEventListener('keyup', addTask)
+inputButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    addHTML();
+    removeTask();
+});
